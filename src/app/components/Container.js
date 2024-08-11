d262e19e1043cf909828ee11f7bf2569fbc88ecb
JavenVernetGeoplus <javen.vernet@geo-plus.com>
2024-08-10 22:28:30 -0400

    Card clicked

--- src/app/components/Container.js
+++ src/app/components/Container.js
@@ -1,33 +1,47 @@
+"use client";
 import MusicCard from "./MusicCard";
+import { useState } from "react";
 
-export default function Container() {
-  const song = undefined;
+function Container() {
+  const [click, setClick] = useState(null);
   const choices = [
     {
       id: 1,
       image: "/kenCover.jpg",
-      title: "Yale",
+      title: "1",
     },
     {
       id: 2,
       image: "/kenCover.jpg",
-      title: "Yale",
+      title: "2",
     },
     {
       id: 3,
       image: "/kenCover.jpg",
-      title: "Yale",
+      title: "3",
     },
   ];
+
+  function CardClicked(title) {
+    console.log(title);
+    setClick(title);
+  }
 
   return (
     <div className="flex flex-col items-center">
       Music
       <div className="flex flex-row gap-20 mt-20">
-        {choices.map((music) => {
-          <MusicCard image={music.image} title={music.title} />;
-        })}
+        {choices.map((music) => (
+          <MusicCard
+            image={music.image}
+            title={music.title}
+            key={music.id}
+            onClick={CardClicked}
+            clicked={click === music.title}
+          />
+        ))}
       </div>
     </div>
   );
 }
+export default Container;
--- src/app/components/MusicCard.js
+++ src/app/components/MusicCard.js
@@ -1,7 +1,15 @@
+"use client";
 import Image from "next/image";
-function MusicCard({ image, title }) {
+function MusicCard({ image, title, onClick, clicked }) {
+  const styleTrue = "border-green-400";
+  const styleDefault = "border-white";
   return (
-    <div className="border-white border-2 w-fit p-4 text-center rounded-lg cursor-pointer">
+    <div
+      className={` border-2 w-fit p-4 text-center rounded-lg cursor-pointer ${
+        clicked ? styleTrue : styleDefault
+      }`}
+      onClick={() => onClick(title)}
+    >
       <Image src={image} alt="song image" width={150} height={150} />
       <p>{title}</p>
     </div>
